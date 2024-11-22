214cdf88ae2faa16004a33de3fa6cd9316dfbbb4
MontaserMohamed <montaser.hassan.bns@gmail.com>
2024-11-22 22:46:59 +0200

    modify on role at activity and postman collection

--- server/src/Routes/User/activity/activity.routes.ts
+++ server/src/Routes/User/activity/activity.routes.ts
@@ -4,13 +4,14 @@ import express from 'express';
 import validation from '../../../Validations/validationHandler.validation';
 import { ActivityController, } from '../../../Controllers/index.controller';
 import { activityValidator } from '../../../Validations/activity.validation';
+import checkRole from '../../../Middlewares/check-role.middleware';
 
 
 
 const activityRouter = express.Router();
 
 
-activityRouter.post('/', validation(activityValidator.createActivity), ActivityController.createActivity);
+activityRouter.post('/', checkRole(['teacher',]), validation(activityValidator.createActivity), ActivityController.createActivity);
 
 activityRouter.get('/', ActivityController.getAllActivities);
 activityRouter.get('/:activityId', validation(activityValidator.getActivity), ActivityController.getActivityData);
